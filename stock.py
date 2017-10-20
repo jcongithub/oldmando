c2a1f9da6525c67099600dbeaf31a2a541320513
jcongithub <jc100102@gmail.com>
2017-10-20 17:40:01 -0400

    commit

--- stock.py
+++ stock.py
@@ -1,5 +1,6 @@
 from dao import conn
 from datetime import datetime
+import pandas as pd
 
 class Stock:
 	def __init__(self, ticker):
@@ -33,10 +34,74 @@ class Stock:
 		cur.close()
 		return prices
 
-if __name__ == '__main__':
+	def list():
+		sql = 'select * from stocks'
+		cur = conn.execute(sql, {})
+		rows = cur.fetchall()
+		stocks = [{
+			'ticker' 	: row[0],
+			'name'   	: row[1],
+			'industry'	: row[2],
+			'sector'	: row[3],
+			'size'		: row[4],
+			'exchange'	: row[5]
+		} for row in rows]
 
-	adm = Stock('TSLA')
+		return stocks
 
-	print(str(adm))
-	print(str(adm.after(datetime(2017, 7, 31))))
-	print(str(adm.after(datetime(2017, 8, 5))))
+	def current_earning(tickers):
+		ticker_list = str(tickers)[1:-1]
+		print('ticker_list:{}'.format(ticker_list))
+		sql = "select * from earnings where ticker in ({}) group by ticker having date=max(date)".format(ticker_list);
+		print('sql:{}'.format(sql))
+		cur = conn.execute(sql, {})
+		rows = cur.fetchall()
+		earnings = [{
+			'ticker' 	: row[0],
+			'date'		: row[1],
+			'estimate'	: row[2],
+			'period'	: row[3],
+			'reported'	: row[4],
+			'surprise1'	: row[5],
+			'surprise2'	: row[6]
+		} for row in rows]
+
+		cur.close()
+		return earnings;
+
+	def earning_on(date):
+		sql = "select * from earnings where date = '{}'".format(date)
+		print(sql)
+		cur = conn.execute(sql, {})
+		rows = cur.fetchall()
+		cur.close()
+
+		earnings = [{
+			'ticker' 	: row[0],
+			'date'		: row[1],
+			'estimate'	: row[2],
+			'period'	: row[3],
+			'reported'	: row[4],
+			'surprise1'	: row[5],
+			'surprise2'	: row[6]
+		} for row in rows]
+
+		return earnings
+
+
+	def test():
+		adm = Stock('TSLA')
+		print(str(adm))
+		print(str(adm.after(datetime(2017, 7, 31))))
+		print(str(adm.after(datetime(2017, 8, 5))))
+
+		print(pd.DataFrame(Stock.list()))
+		df = pd.DataFrame(Stock.current_earning(['amd', 'tsla', 'kors', 'nflx']))
+		print(df.set_index('ticker'))
+
+		df = pd.DataFrame(Stock.earning_on('2017-10-16'))
+		df = df.set_index(['ticker'])
+		print(df)
+
+#if __name__ == '__main__':
+
